c67fcf13892c97e7b5530d3076441b0fd78a4e87
Andrés Bretón <bretonics@users.noreply.github.com>
2020-08-12 10:53:08 -0400

    add stop emoji

---
+++ bin/restore_backup.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+DIRECTORIES=('Movies' 'Music' 'Pictures')
+BACKUP_PATH=${1}
+
+BIN_PATH="$(dirname ${BASH_SOURCE})"
+DIR_PATH="$(dirname $BIN_PATH)"
+
+source ${DIR_PATH}/lib/functions
+
+if [ $# -eq 0 ]; then
+    BACKUP_PATH=${PWD}
+fi
+
+# Ensure properly formated path - spaces to '\ ' and no forward slash
+BACKUP_PATH=$(echo ${BACKUP_PATH} | sed 's/[[:space:]]/\\ /g')
+BACKUP_PATH=$(echo ${BACKUP_PATH} | sed -E 's/\/?$//')
+
+# Start Logs
+# loggers
+
+# Copy over all content
+for DIR in ${DIRECTORIES[@]}; do
+    BACKUP_DIR="${BACKUP_PATH}/${DIR}/"
+    section "Syncing Backup Directory '${DIR}'"
+    echo rsync -avi --progress "${BACKUP_DIR}" ~/${DIR}/
+    rsync -avi --progress "${BACKUP_DIR}" ~/${DIR}/
+done
