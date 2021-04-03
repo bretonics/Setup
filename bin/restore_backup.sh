116bf76a4c5234246a7898f602ebecb1b48742c7
Andrés Bretón <bretonics@users.noreply.github.com>
2021-04-03 11:57:15 -0400

    add restoring backup script

--- bin/restore_backup.sh
+++ bin/restore_backup.sh
@@ -13,7 +13,7 @@ if [ $# -eq 0 ]; then
     BACKUP_PATH=${PWD}
 fi
 
-# Ensure properly formated path - spaces to '\ ' and no forward slash
+# Ensure properly formatted path - spaces to '\ ' and no forward slash
 BACKUP_PATH=$(echo ${BACKUP_PATH} | sed 's/[[:space:]]/\\ /g')
 BACKUP_PATH=$(echo ${BACKUP_PATH} | sed -E 's/\/?$//')
 
@@ -23,7 +23,11 @@ loggers
 # Copy over all content
 for DIR in ${DIRECTORIES[@]}; do
     BACKUP_DIR="${BACKUP_PATH}/${DIR}/"
-    section "Syncing Backup Directory '${DIR}'"
+    subsection "Syncing Backup Directory '${DIR}'"
     echo -e "rsync -avi --progress "${BACKUP_DIR}" ~/${DIR}/ \n\n"
     caffeinate rsync -avi --progress "${BACKUP_DIR}" ~/${DIR}/
 done
+
+# Assert Time Machine Backups Use Same Backup Folder
+sudo tmutil inheritbackup /Volumes/Backups/Backups.backupdb/Mac\ mini
+sudo tmutil associatedisk -a /Volumes/Backups /Volumes/Backups/Backups.backupdb/Mac\ mini/Latest/Macintosh\ HD
