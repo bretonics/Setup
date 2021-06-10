85149b7a493eb7290fd5692ed5cab22d028da955
Andrés Bretón <bretonics@users.noreply.github.com>
2021-06-10 15:16:36 -0400

    init pmset

--- bin/settings_defaults.sh
+++ bin/settings_defaults.sh
@@ -105,3 +105,7 @@ defaults write com.apple.TextEdit RichText -int 0
 ask "Open and save files as UTF-8 in TextEdit"
 defaults write com.apple.TextEdit PlainTextEncoding -int 4
 defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+#- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+# ENERGY SAVING
+sudo pmset -a displaysleep 15
